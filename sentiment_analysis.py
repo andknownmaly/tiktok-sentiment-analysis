84ae589b0a306ea4afc5ef30a2e72dd4efeb3f0b
Danang Tri Atmaja <114817148+dword32bit@users.noreply.github.com>
2025-01-02 22:01:05 +0700

    Update sentiment_analysis.py

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -139,8 +139,8 @@ if analyze_button:
                 for comment_data in comments:
                     comment = comment_data["comment"]
                     pre = preprocess_comment_with_language_detection(comment)
-                    #st.write(pre)
-                    sentiment = analyze_sentiment(pre)
+                    st.write(pre)
+                    sentiment = analyze_sentiment(comment) #harusnya di isi variable pre
                     sentiment_counts[sentiment["overall_sentiment"]] += 1
 
                 # Display sentiment analysis results as a bar chart
